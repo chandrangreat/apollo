0ef2a0a0cc31c02793f069d60848d8c1547b8e21
Ravi Chandran <chandrangreatfree@gmail.com>
2018-05-25 23:59:17 +0530

    #8 GraphQL Schema Files - Full-stack GraphQL with Apollo, Meteor & React

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 
-const App = ({data}) => <h1>{data.hi}</h1>;
+const App = ({ data }) => <h1>{data.hi}</h1>;
 
 const hiQuery = gql`
 {
